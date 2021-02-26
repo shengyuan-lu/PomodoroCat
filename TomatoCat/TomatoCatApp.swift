60b0c04532c3de51210caddccd5330dc0adaed0e
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-02-26 14:28:48 -0800

    Initial Commit

--- TomatoCat/TomatoCatApp.swift
+++ TomatoCat/TomatoCatApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TomatoCatApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- TomatoCat/Views/MainView.swift
+++ TomatoCat/Views/MainView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
