7ee5ffdc791e82476cc0c151453eb585cef53fa9
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-02-26 14:32:31 -0800

    Timer Branch Created

---
+++ TomatoCat/Views/TimerView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
