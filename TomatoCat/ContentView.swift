5a502e42f49209686c9294058be1aaa6e221ab0e
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-02-26 14:19:07 -0800

    Initial Commit

---
+++ TomatoCat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TomatoCat
+//
+//  Created by Shengyuan Lu on 2/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
